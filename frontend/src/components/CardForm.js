62b69d970bc2efef71ebb5106689ed11a18d5a68
Bibhor <bibhorsharma7@gmail.com>
2021-07-14 13:46:47 +1000

    added more fields to the form

--- frontend/src/components/CardForm.js
+++ frontend/src/components/CardForm.js
@@ -1,29 +1,61 @@
 import React, { useState } from 'react';
 import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
-import {TextField} from '@material-ui/core';
+import {Button, TextField} from '@material-ui/core';
 
 function CardForm() {
   const [number, setNumber] = useState('')
   const [name, setName] = useState('')
   const [expiry, setExpiry] = useState('')
   const [cvv, setCvv] = useState('')
+  const [focus, setFocus] = useState('')
 
-  function handleChange(e) {
-    setName(e.target.value);
-  }
+  
 
   return (
-    <div>
-      <h1>Hello there</h1>
-      <h2>General Kenobi!</h2>
+    <div className="card-form">
       <Cards
         name={name}
         number={number}
         expiry={expiry}
-        cvv={cvv}
+        cvc={cvv}
+        focused={focus}
       />
-      <TextField placeholder="Cardholder Name" onChange={handleChange} />
+      <div className="form-container">
+        <TextField className="form-item" variant="outlined"
+          size="small"
+          label="Card Number"
+          placeholder="Card Number"
+          onFocus={() => {setFocus('number')}}
+          onChange={(e) => {setNumber(e.target.value)}}
+        />
+        <TextField className="form-item" variant="outlined"
+          label="Cardholder Name"
+          size="small"
+          placeholder="Cardholder Name"
+          onFocus={() => {setFocus('name')}}
+          onChange={(e) => {setName(e.target.value)}}
+        />
+
+        <div className="form-item">
+          <TextField variant="outlined" type="date" size="small"
+            label="Expiry Date"
+            onFocus={() => {setFocus('expiry')}}
+          />
+          <TextField variant="outlined" type="number"
+            size="small"
+            placeholder="CVV"
+            label="CVC"
+            onFocus={() => {setFocus('cvc')}}
+            onChange={(e) => {setCvv(e.target.value)}}
+          />
+        </div>
+
+        <Button className="form-item" variant="contained"
+          color="primary" onClick={console.log("submit clicked")}>
+            Submit
+        </Button>
+      </div>
     </div>
   );
 }
