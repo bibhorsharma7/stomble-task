43b4948a68e5cca0d2bc957adb44a3301e524341
Bibhor <bibhorsharma7@gmail.com>
2021-07-16 13:54:44 +1000

    added parameter validation

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -10,6 +10,38 @@ const app = express();
 
 app.use(cors());
 
+function validate(card) {
+  if (card.name !== null && card.name === "")
+    return false;
+  
+  const nameRe = /[0-9]$/;
+  if (nameRe.test(card.name))
+    return false;
+  
+  if (card.number !== null && card.number === "")
+    return false;
+
+  const nRe = /^([0-9]{16})$/;
+  if (! nRe.test(card.number))
+    return false;
+  
+  if (card.cvc !== null && card.cvc === "")
+    return false;
+
+  const cRe = /^[1-9][0-9]{2,3}$/;
+  if (! cRe.test(card.cvc))
+    return false;
+
+  if (card.expiry !== null && card.expiry === "")
+    return false
+    
+  const exRe = /^(0[1-9]|1[0-2])\/?((0[1-9])|([1-9][0-9]))$/;
+  if (! exRe.test(card.expiry))
+    return false;
+
+  return true;
+}
+
 app.get("/", async (req, resp) => {
   const result = await admin.firestore().collection("cards").get();
 
@@ -23,8 +55,27 @@ app.get("/", async (req, resp) => {
   resp.status(200).send(JSON.stringify(cards))
 });
 
+
 app.post("/", async (req, resp) => {
   const card = req.body.data;
+  if (card == null) {
+    resp.status(500).send('Bad Request');
+    return;
+  }
+
+  let inDB = await admin.firestore().collection("cards").get()  
+  inDB.forEach((doc) => {
+    let data = doc.data();
+    if (card.number === data.number) {
+      resp.status(303).send('This Card already exists');
+      return;
+    }
+  })
+
+  if (! validate(card)) {
+    resp.status(400).send('Please enter valid card details');
+    return;
+  }
 
   await admin.firestore().collection("cards").add(card);
   resp.status(200).send('Success');
